6a97b34c857cc2eb449812533dd7b4d563506cd7
VishalCataram <32086560+VishalCataram@users.noreply.github.com>
2017-09-19 00:13:46 -0500

    Update program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <conio.h>
 #include <time.h>
 #include <sys/time.h>
 
@@ -7,27 +8,22 @@ using namespace std;
 
 int main() {
        
-         int a,n;
-        int k=0,b=1;
+        int a;
+        int n;
+        int k=0;
+        int b=1;
         double t;
         struct timeval s,e;
         printf("Enter a:");
         scanf("%d",&a);
         printf("Enter n:");
         scanf("%d",&n);
-      
         gettimeofday(&s,0);
-        // calculate nth power of a
-        while (k < n )
-        {
-        k = k + 1;
-        b=b*a;
-        printf("\n b is : %d\n", b);
-        }
+        
        gettimeofday(&e, 0);
        printf("\n b is : %d\n", b);
        //calculate time taken
        t=(e.tv_sec + e.tv_usec*0.000001) - (s.tv_sec + s.tv_usec*0.000001);
-         printf("time taken : %f\n", t);
+        printf("time taken : %f\n", t);
         return 0;
 }
