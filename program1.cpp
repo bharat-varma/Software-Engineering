bfea64aaf88bedd09f726c3b37226f98ce856012
akshayg9 <32023195+akshayg9@users.noreply.github.com>
2017-09-18 21:53:03 -0700

    Update program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -11,11 +11,7 @@ int main() {
         int k=0,b=1;
         double t;
         struct timeval s,e;
-        printf ("Enter a: ");
-        scanf ("%d", &a);
-
-        printf ("Enter n: ");
-        scanf ("%d", &n);
+      
         gettimeofday(&s,0);
         // calculate nth power of a
         while (k < n )
