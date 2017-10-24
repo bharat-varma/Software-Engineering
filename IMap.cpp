126c4ff3f0007e15e3454b8dcf84daec6ca41333
akshayg9 <32023195+akshayg9@users.noreply.github.com>
2017-10-23 19:12:49 -0700

    Update IMap.cpp

--- IMap.cpp
+++ IMap.cpp
@@ -61,3 +61,86 @@ namespace iProlog
 	  {
 		return nullptr != map.erase(key);
 	  }
+	  int size()
+	  {
+		const Iterator<K> I = map.keySet().begin();
+		int s = 0;
+		while (I->hasNext())
+		{
+		  constexpr K key = I->next();
+		  IntMap * const vals = get(key);
+		  s += vals->size();
+			I++;
+		}
+		return s;
+	  }
+
+	  Set<K> *keySet()
+	  {
+		return map.keySet();
+	  }
+
+	  Iterator<K> *keyIterator()
+	  {
+		return keySet()->begin();
+	  }
+
+	  std::wstring toString() override
+	  {
+
+		return map.toString();
+	  }
+
+	  // specialization for array of int maps
+
+	  static std::vector<IMap<int>*> create(int const l)
+	  {
+		IMap<int> * const first = new IMap<int>();
+
+		std::vector<IMap<int>*> imaps = static_cast<std::vector<IMap<int>*>>(java::lang::reflect::Array::newInstance(first->getClass(), l));
+		//new IMap[l];
+		imaps[0] = first;
+		for (int i = 1; i < l; i++)
+		{
+		  imaps[i] = new IMap<int>();
+		}
+		return imaps;
+	  }
+
+	  static bool put(std::vector<IMap<int>*> &imaps, int const pos, int const key, int const val)
+	  {
+		return imaps[pos]->put(boost::optional<int>(key), val);
+	  }
+
+	  static std::vector<int> get(std::vector<IMap<int>*> &iMaps, std::vector<IntMap*> &vmaps, std::vector<int> &keys)
+	  {
+		constexpr int l = iMaps.size();
+		const std::vector<IntMap*> ms = std::vector<IntMap*>();
+		const std::vector<IntMap*> vms = std::vector<IntMap*>();
+
+		for (int i = 0; i < l; i++)
+		{
+		  constexpr int key = keys[i];
+		  if (0 == key)
+		  {
+			continue;
+		  }
+		  //Main.pp("i=" + i + " ,key=" + key);
+		  IntMap * const m = iMaps[i]->get(boost::optional<int>(key));
+		  //Main.pp("m=" + m);
+		  ms.push_back(m);
+		  vms.push_back(vmaps[i]);
+		}
+		std::vector<IntMap*> ims(ms.size());
+		std::vector<IntMap*> vims(vms.size());
+
+		for (int i = 0; i < ims.size(); i++)
+		{
+		  IntMap * const im = ms[i];
+		  ims[i] = im;
+		  IntMap * const vim = vms[i];
+		  vims[i] = vim;
+		}
+
+		
+
